cd21cf9940044554d9fd31b8788ea93275881b9a
Drew Capener <drew_capener@byu.edu>
2020-07-10 14:42:14 -0600

    rudimentary video controls working

--- client/HigherEd/src/app/screen/screen.component.ts
+++ client/HigherEd/src/app/screen/screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-screen',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScreenComponent implements OnInit {
 
+  @ViewChild('video', {static: true }) video: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  playVideo() {
+    this.video.nativeElement.play();
+  }
+
+  pauseVideo() {
+    this.video.nativeElement.pause();
   }
 
 }
