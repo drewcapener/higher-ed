0d664bb0bb63dad92651794903241bf112b867ee
Drew Capener <drew_capener@byu.edu>
2020-07-21 17:21:53 -0600

    Started work on video thumbnails

--- client/HigherEd/src/app/document/document.component.ts
+++ client/HigherEd/src/app/document/document.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-document',
@@ -9,7 +9,6 @@ export class DocumentComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
