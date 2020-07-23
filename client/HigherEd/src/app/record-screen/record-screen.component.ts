5151aacc1fe791f48025779dd173d45e845346e5
Drew Capener <drew_capener@byu.edu>
2020-07-23 15:37:07 -0600

    Basic functionality for recording in lecture studio is wired up

--- client/HigherEd/src/app/record-screen/record-screen.component.ts
+++ client/HigherEd/src/app/record-screen/record-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-record-screen',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecordScreenComponent implements OnInit {
 
+  @ViewChild('video', { static: true }) video: ElementRef;
+
+  recording: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.recording = false;
+  }
+
+  startStream(): void {
+    this.recording = true
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(
+      (stream) => {
+        this.video.nativeElement.srcObject = stream;
+      }
+    )
+  }
+
+  endStream(): void {
+    this.recording = false;
+    let stream = this.video.nativeElement.srcObject;
+    let tracks = stream.getTracks();
+
+    for (var i = 0; i < tracks.length; i++) {
+      var track = tracks[i];
+      track.stop();
+    }
+
+    this.video.nativeElement.srcObject = null;
   }
 
 }
