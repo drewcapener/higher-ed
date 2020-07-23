f10de1c03963b05428fe95bc6fcf155f4de1a443
Drew Capener <drew_capener@byu.edu>
2020-07-23 13:03:06 -0600

    Basic login wired up

--- client/HigherEd/src/app/username-password/username-password.component.ts
+++ client/HigherEd/src/app/username-password/username-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-username-password',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsernamePasswordComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  password: string;
+  outline: string;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  login(): void {
+    if (this.username === 'drew' && this.password === 'drew') {
+      this.router.navigate(['/class-archives']);
+    } else {
+      console.log('error logging in');//fixme
+    }
   }
 
 }
